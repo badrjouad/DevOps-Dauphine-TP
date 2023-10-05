a6757e74269d61d25c4bbf460eeac5ee59427d52
badrjouad <jouad324@gmail.com>
2023-10-05 09:57:09 +0000

    part 3

--- environnements/main.tf
+++ environnements/main.tf
@@ -26,3 +26,44 @@ resource "google_sql_user" "wordpress" {
    instance = "main-instance"
    password = "ilovedevops"
 }
+template {
+   spec {
+      containers {
+        image = "us-central1-docker.pkg.dev/artifact-399517/website-tools/website-image"
+        ports {
+        container_port = 80
+        }
+     }
+   }
+
+   
+
+   metadata {
+      annotations = {
+            "run.googleapis.com/cloudsql-instances" = "artifact-399517:us-central1:main-instance"
+      }
+   }
+}
+
+traffic {
+   percent         = 100
+   latest_revision = true
+}
+
+
+data "google_iam_policy" "noauth" {
+   binding {
+      role = "roles/run.invoker"
+      members = [
+         "allUsers",
+      ]
+   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
+}
